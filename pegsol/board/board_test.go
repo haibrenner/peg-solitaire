b21afd0ceee4e4c6b075bdc3faf4d778cada8916
haibrenner <haibrenner@gmail.com>
2026-04-07 15:24:54 +0300

    add test for MatrixState and CompactState conversions

--- pegsol/board/board_test.go
+++ pegsol/board/board_test.go
@@ -3,9 +3,10 @@ package board
 import (
 	"testing"
 
+	"peg_solitaire/pegsol/matrixstate"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"peg_solitaire/pegsol/matrixstate"
 )
 
 const inputsDir = "../inputs/"
@@ -15,4 +16,20 @@ func TestAllPossibleMoves(t *testing.T) {
 	require.NoError(t, err)
 	b := NewBoard(ms)
 	assert.Len(t, b.Moves, 76)
+}
+
+func TestTranslateMatrixRoundtrip(t *testing.T) {
+	ms, err := matrixstate.ReadInput(inputsDir + "standard_english.txt")
+	require.NoError(t, err)
+	ms.Cells[0][0] = matrixstate.CellPeg
+
+	b := NewBoard(ms)
+
+	cs, err := b.TranslateMatrixToCompactState(ms)
+	require.NoError(t, err)
+
+	got, err := b.TranslateCompactToMatrixState(cs)
+	require.NoError(t, err)
+
+	assert.Equal(t, ms, got)
 }
