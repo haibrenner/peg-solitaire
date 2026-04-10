ba32dd62b3b586a070b2c4b4917d2b3463f200b5
haibrenner <haibrenner@gmail.com>
2026-04-11 00:52:45 +0300

    add test for the bfs solver

---
+++ pegsol/bfs/bfs_solver_test.go
@@ -0,0 +1,43 @@
+package bfs
+
+import (
+	"testing"
+
+	"peg_solitaire/pegsol/board"
+	"peg_solitaire/pegsol/matrixstate"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const inputsDir = "../inputs/"
+
+func prepareForSolve(t *testing.T, filename string) (board.CompactState, []*board.CompactJump) {
+	t.Helper()
+	ms, err := matrixstate.ReadInput(inputsDir + filename)
+	require.NoError(t, err)
+
+	b, err := board.NewBoard(ms)
+	require.NoError(t, err)
+
+	jumps, err := b.TranslateAllCoordJumpsToCompact()
+	require.NoError(t, err)
+
+	initial, err := b.TranslateMatrixToCompactState(ms)
+	require.NoError(t, err)
+
+	return initial, jumps
+}
+
+func TestSolve_TooSimple(t *testing.T) {
+	initial, jumps := prepareForSolve(t, "test_too_simple.txt")
+	solution := Solve(initial, jumps, 0)
+	require.NotNil(t, solution)
+	assert.Len(t, solution, 2)
+}
+
+func TestSolve_TooSimpleUnsolvable(t *testing.T) {
+	initial, jumps := prepareForSolve(t, "test_too_simple_unsolvable.txt")
+	solution := Solve(initial, jumps, 0)
+	assert.Nil(t, solution)
+}
